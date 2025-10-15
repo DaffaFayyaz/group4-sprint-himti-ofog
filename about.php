9269440b6bdd96889d3438b9d015a24d0bd06c4d
daffafayyaz <daffafyyaz@gmail.com>
2025-10-15 20:27:41 +0700

    feat: modal pop video di about us (iframe sizenya masi kecil bet)

--- about.php
+++ about.php
@@ -142,7 +142,7 @@ Tenggara dalam bidang teknologi dan informatika."</p>
                         </div>
                     </div>
                     <div class="hidden lg:block cd-img-container">
-                        <img  id="openEducationModal" class="com-logo cd-img" src="assets/img/about/komsat-logo-2025.svg" alt="">
+                        <img class="com-logo cd-img open-modal" src="assets/img/about/komsat-logo-2025.svg" alt="" data-video="https://www.youtube.com/embed/qSEqg0m1cAU">
                     </div>
                     <div class="cd-img-container">
                         <img class="cd-img" src="assets/img/about/ae-logo-2025.svg" alt="">
@@ -152,27 +152,6 @@ Tenggara dalam bidang teknologi dan informatika."</p>
                         </div>
                     </div>
                 </div>
-            </div>
-
-            <!-- Modal -->
-            <div id="educationModal" class="fixed inset-0 bg-black bg-opacity-60 hidden justify-center items-center z-50">
-              <div class="bg-white rounded-2xl shadow-2xl overflow-hidden max-w-3xl w-full relative">
-                <!-- Close button -->
-                <button id="closeModalBtn" class="absolute top-3 right-3 text-gray-700 hover:text-red-500 text-2xl font-bold">&times;</button>
-
-                <!-- Video container -->
-                <div class="aspect-video w-full">
-                  <iframe
-                    id="educationVideo"
-                    class="w-full h-full"
-                    src="https://www.youtube.com/embed/qSEqg0m1cAU"
-                    title="Commission 1: Education Video"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen>
-                  </iframe>
-                </div>
-              </div>
             </div>
 
             <div class="py-8 border-b border-gray-100">
@@ -190,7 +169,7 @@ Tenggara dalam bidang teknologi dan informatika."</p>
                         </div>
                     </div>
                     <div class="hidden lg:block cd-img-container">
-                        <img class="com-logo cd-img" src="assets/img/about/komdu-logo-2025.svg" alt="">
+                        <img class="com-logo cd-img open-modal" src="assets/img/about/komdu-logo-2025.svg" alt="" data-video="https://www.youtube.com/embed/qSEqg0m1cAU">
                     </div>
                     <div class="cd-img-container">
                         <img class="cd-img" src="assets/img/about/himticare-logo-2025.svg" alt="">
@@ -221,7 +200,7 @@ Tenggara dalam bidang teknologi dan informatika."</p>
                         </div>
                     </div>
                     <div class="hidden lg:block cd-img-container">
-                        <img class="com-logo cd-img" src="assets/img/about/komtig-logo-2025.svg" alt=""  >
+                        <img class="com-logo cd-img open-modal" src="assets/img/about/komtig-logo-2025.svg" alt="" data-video="https://www.youtube.com/embed/qSEqg0m1cAU">
                     </div>
                     <div class="cd-img-container">
                         <img class="cd-img" src="assets/img/about/webdev-logo-2025.svg" alt="">
@@ -250,7 +229,7 @@ Tenggara dalam bidang teknologi dan informatika."</p>
                         </div>
                     </div>
                     <div class="hidden lg:block cd-img-container">
-                        <img class="w-75 com-logo cd-img" src="assets/img/about/kompat-logo-2025.svg" alt="">
+                        <img class="w-75 com-logo cd-img open-modal" src="assets/img/about/kompat-logo-2025.svg" alt="" data-video="https://www.youtube.com/embed/qSEqg0m1cAU">
                     </div>
                     <div class="cd-img-container">
                         <img class="cd-img" src="assets/img/about/hrd-logo-2025.svg" alt="">
@@ -330,42 +309,61 @@ Tenggara dalam bidang teknologi dan informatika."</p>
                 </div>
             </div>
         </div>
-    </div>
+      </div>
 
-    
+
+      <!--  Modal pop-up video for each Commissions-->
+      <div id="universalModal" class="fixed inset-0 bg-black bg-opacity-80 hidden justify-center items-center z-50">
+        <div class="relative w-[95vw] md:w-[90vw] lg:w-[85vw] xl:w-[80vw] h-[80vh] bg-black rounded-2xl shadow-2xl overflow-hidden flex flex-col justify-center items-center">
+          <!-- Close Button -->
+          <button id="closeUniversalModal" class="absolute top-4 right-6 text-white hover:text-red-400 text-4xl font-bold z-10">&times;</button>
+
+          <!-- Video Container -->
+          <iframe
+            id="modalVideo"
+            class="w-full h-full rounded-2xl"
+            src=""
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen>
+          </iframe>
+        </div>
+      </div>
+
     <?php require_once('components/footer.php') ?>
 
     <!-- <script src="script.js"></script> -->
-<script>
-  const openBtn = document.getElementById('openEducationModal');
-  const modal = document.getElementById('educationModal');
-  const closeBtn = document.getElementById('closeModalBtn');
-  const video = document.getElementById('educationVideo');
 
-  // Replace with your actual video link
-  const videoSrc = "https://www.youtube.com/embed/qSEqg0m1cAU";
+   <script>
+     const modal = document.getElementById('universalModal');
+     const closeBtn = document.getElementById('closeUniversalModal');
+     const videoFrame = document.getElementById('modalVideo');
+     const triggers = document.querySelectorAll('.open-modal');
 
-  openBtn.addEventListener('click', () => {
-    modal.classList.remove('hidden');
-    modal.classList.add('flex');
-    video.src = videoSrc; // start video when opened
-  });
+     triggers.forEach(trigger => {
+       trigger.addEventListener('click', () => {
+         const videoUrl = trigger.getAttribute('data-video');
+         modal.classList.remove('hidden');
+         modal.classList.add('flex');
+         videoFrame.src = videoUrl + '?autoplay=1';
+       });
+     });
 
-  closeBtn.addEventListener('click', () => {
-    modal.classList.add('hidden');
-    modal.classList.remove('flex');
-    video.src = ""; // stop video when closed
-  });
+     closeBtn.addEventListener('click', () => {
+       modal.classList.add('hidden');
+       modal.classList.remove('flex');
+       videoFrame.src = "";
+     });
 
-  // Optional: close when clicking outside the modal
-  modal.addEventListener('click', (e) => {
-    if (e.target === modal) {
-      modal.classList.add('hidden');
-      modal.classList.remove('flex');
-      video.src = "";
-    }
-  });
-</script>
+     modal.addEventListener('click', (e) => {
+       if (e.target === modal) {
+         modal.classList.add('hidden');
+         modal.classList.remove('flex');
+         videoFrame.src = "";
+       }
+     });
+   </script>
+
 </body>
 
 </html>
